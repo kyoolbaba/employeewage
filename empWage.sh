1e621c96f21a39ce69480169505f72486b2c6621
kyoolbaba <kyoolbaba@gmail.com>
2020-04-10 18:23:55 +0530

    calculating monthlywages assuming 20 working days

--- empWage.sh
+++ empWage.sh
@@ -3,15 +3,19 @@
 echo "Welcome to employeewage computation"
 wagePerHour=20
 declare wage
+workingdays=20
 isPresent=$((RANDOM%2))
 case $isPresent in
     1 ) echo "Employee is present" 
         echo wage=$((wagePerHour * 8)) 
+        echo workingdays=$((wage * 20))
      ;;
     0 ) echo "Employee is absent"
     echo wage=0  
+    echo workingdays=0
     ;;
 esac
+
 
 
 
