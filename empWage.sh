08b6a07a4566f1817f2e5003fab47b049891a6d2
kyoolbaba <kyoolbaba@gmail.com>
2020-04-10 15:48:42 +0530

    added logic for calculating the wage

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
 echo "Welcome to employeewage computation"
-
+wagePerHour=20
+declare wage
 isPresent=$((RANDOM%2))
 if [[ $isPresent -eq 1 ]]; then
     echo "Employee is Present"
 else
     echo "Employee is Absent"
 fi
+if [[ $isPresent -eq 1 ]]; then
+    echo  wage=$((wagePerHour*8))
+else
+    echo wage=0
+fi
+
