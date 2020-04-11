1810fb74477940f131fa1a99dccd7b46e11d7842
kyoolbaba <kyoolbaba@gmail.com>
2020-04-11 12:24:49 +0530

    Refactoring

--- empWage.sh
+++ empWage.sh
@@ -2,32 +2,32 @@
 
 echo "Welcome to employeewage computation"
 wagePerHour=20
-declare dailywage
-declare totalwage
-workingdays=20
-workinghours=100
+declare dailyWage
+declare totalWage
+WORKING_DAYS=20
+WORKING_HOURS=100
 isPresent=$((RANDOM%2))
 case $isPresent in
     1 ) echo "Employee is present" 
-        echo dailywage=$((wagePerHour * 8)) 
-        echo workingdays=$((wage * 20))
+        echo wage=$((wagePerHour * 8)) 
+        echo dialyWage=$((wage * 20))
      ;;
     0 ) echo "Employee is absent"
     echo wage=0  
-    echo workingdays=0
+    echo dailyWage=0
     ;;
 esac
 
-if [[ $workingdays -ge 20 ]]; then
-    echo totalwage=$((wagePerHour * workingdays))
+if [[ $WORKING_DAYS -ge 20 ]]; then
+    echo totalwage=$((wagePerHour * WORKING_DAYS))
 fi
-if [[ $workinghours -ge 100 ]]; then
-    echo totalwage=$((wagePerHour * workinghours))
+if [[ $WORKING_HOURS -ge 100 ]]; then
+    echo totalwage=$((wagePerHour * WORKING_HOURS))
 fi
 
 getWorkinghours ()
 {
-    echo Printing working hours  that is $workinghours hours
+    echo Printing working hours  that is $WORKING_HOURS hours
 }
 getWorkinghours
 
